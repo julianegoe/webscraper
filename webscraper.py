4490fe81881ad5f62ea819047e5d222d83ddff02
juliane-dpa <goersch.juliane@dpa.com>
2019-03-19 15:28:09 +0100

    added more to gitignore, chanded code of webscaper.py

--- webscraper.py
+++ webscraper.py
@@ -6,25 +6,11 @@ url = 'https://daserste.ndr.de/annewill/archiv/index.html' #Sendungsarchiv von A
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-#jetzt folgen verschiedene Ideen wie ich an den Text komme. In den <div> mit der Class "Teaser" sind <p> mit der Class "teasertext". 
-#Dort stecken die Gäste in einem <a> drin. Die <a> haben aber keine Class, sondern nur einen Link href und einen title.
+teaserli = []	
+for element in content.find_all("div", class_= "teaser"):
+	for a_element in element.find_all("a"):
+		teaserli.append(a_element.get_text()
+		
 
-
-#Hier tu ich den Inhalte eines jeden <div class="teaser"> in eine Liste, 
-#loope da durch und gucke, was davon den <a> tag hat. Klappt aber überhaupt nicht die Idee. 
-#teaser = content.find_all('div', class_= "teaser") 
-#teaserli = []
-#for x in teaser:
-#	teaserli.append(x)
-	#print(teaserli)
-#for y in teaserli:
-#	if y == "a":
-		#print(y)
-
-
-#hier bekomme ich zwar nur den Text aber neun mal den gleichen der ersten Gäste.
-
-gast = content.find_all('div', class_= "teaser")
-for x in gast: 
-	x = content.find('p', class_= "teasertext").get_text()
-	print(x)
+#with open('willData.json', 'w') as outfile:
+#    json.dump(teaserliste, outfile)
