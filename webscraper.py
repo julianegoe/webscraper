da6773ed1a3ad0080c03c606a4b0f2f8388402b0
juliane-dpa <goersch.juliane@dpa.com>
2019-03-19 15:50:32 +0100

    working code

--- webscraper.py
+++ webscraper.py
@@ -1,4 +1,3 @@
-# Zeilen 2 - 7 sind aus einem Scraping Tutorial
 from bs4 import BeautifulSoup
 import requests
 import json
@@ -7,10 +6,10 @@ response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
 teaserli = []	
-for element in content.find_all("div", class_= "teaser"):
+for element in content.find_all("p", class_= "teasertext"):
 	for a_element in element.find_all("a"):
-		teaserli.append(a_element.get_text()
-		
+		teaserli.append(a_element.get_text())
+		print(teaserli)
 
-#with open('willData.json', 'w') as outfile:
-#    json.dump(teaserliste, outfile)
+with open('willData.json', 'w') as outfile:
+	json.dump(teaserli, outfile)
